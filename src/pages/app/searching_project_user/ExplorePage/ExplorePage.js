beaffeb6c320077204c7eef395c2f39cc13a778f
unknown <marwinnortington@gmail.com>
2020-08-18 23:59:38 +0700

    first commit

---
+++ src/pages/app/searching_project_user/ExplorePage/ExplorePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Components
+import Header from '../../../../components/Header';
+import ProjectType from '../../../../components/ProjectType'
+
+const ExplorePage = props => {
+    return(
+        <div className="container">
+            
+            Hello world!
+        </div>
+    )
+}
+
+export default ExplorePage;
---
+++ src/routes/MainRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+// Pages
+import ExplorePage from '../pages/app/searching_project_user/ExplorePage/ExplorePage'
+
+const MainRoute = ()=> {
+    return(
+        <Router>
+            <Switch>
+                <Route path="" component={ExplorePage} />
+            </Switch>
+        </Router>
+    )
+}
+
+export default MainRoute;
