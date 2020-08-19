ede2c01c37ebc7b2b6358e273c9ad1fd0dd7ec86
Tran Ngoc Tien <ngoctien27122001@gmail.com>
2020-08-19 21:36:22 +0700

    Remove old comments

--- src/pages/app/searching_project_user/ExplorePage.js
+++ src/pages/app/searching_project_user/ExplorePage.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-// Components
-import Header from '../../../../components/Header';
-import ProjectType from '../../../../components/ProjectType'
-
 const ExplorePage = props => {
     return(
         <div className="container">
