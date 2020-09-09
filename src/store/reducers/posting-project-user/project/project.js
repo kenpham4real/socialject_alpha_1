150405dc7e0bd32fb339bc5054e732f8c90b4c2e
unknown <marwinnortington@gmail.com>
2020-09-09 21:42:25 +0700

    Re-implement view-project-recruit-info feature

--- src/store/reducers/posting-project-user/project/project.js
+++ src/store/reducers/posting-project-user/project/project.js
@@ -1,17 +1,30 @@
 import {
-    SET_PROJECT
+    SET_PROJECT_RECRUIT_INFO,
+    SET_PROJECT_BASIC_INFO
 } from '../../../actions/posting-project-user/project/project'
 
 const initialState = {
-    projects: [],
+    projects_basic_info: [],
+    projects_recruit_info: {
+        benefits: [],
+        requirements: []
+    }
 };
 
 export const projectReducer = (state = initialState, action) => {
     switch(action.type){
-        case SET_PROJECT:
+        case SET_PROJECT_BASIC_INFO:
             return{
                 ...state,
-                projects: action.projectData
+                projects_basic_info: state.projects_basic_info.concat(action.projectData)
+            }
+        case SET_PROJECT_RECRUIT_INFO:
+            return{
+                ...state,
+                projects_recruit_info: {
+                    benefits: action.benefits,
+                    requirements: action.requirements
+                }
             }
         default: return state;
     }
