3163008e7f6125f4e5d4720fe77edf715633ef12
unknown <marwinnortington@gmail.com>
2020-09-16 23:34:05 +0700

    Handlie the data from org fetching actions

--- src/store/reducers/posting-project-user/profile/profileReducer.js
+++ src/store/reducers/posting-project-user/profile/profileReducer.js
@@ -1,4 +1,8 @@
-import { CHANGE_TEXT } from "../../../actions/posting-project-user/profile/profileAction";
+import {
+  CHANGE_TEXT, 
+  SET_ORGANIZATION_PROFILE, 
+  SET_ORGANIZATION_PROJECT 
+} from "../../../actions/posting-project-user/profile/profileAction";
 import { fetchProfile_ppu } from "../../../actions/posting-project-user/profile/profileAction";
 
 const initialState = {
@@ -27,6 +31,13 @@ export const profileReducer = (state = initialState, action) => {
         ...state,
         profileKey: action.payload,
       };
+    case SET_ORGANIZATION_PROFILE:
+      return{
+        ...state,
+        profileData: action.profileData,
+        projectData: action.profile_projectData
+      }
+    
     default:
       return {
         projectData: action.type,
