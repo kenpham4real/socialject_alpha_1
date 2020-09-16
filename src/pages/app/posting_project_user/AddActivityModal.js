1ff57219f767435395fa8f6834bd4784ea7234c6
unknown <marwinnortington@gmail.com>
2020-09-16 21:13:17 +0700

    Add choose ad preview image feature

--- src/pages/app/posting_project_user/AddActivityModal.js
+++ src/pages/app/posting_project_user/AddActivityModal.js
@@ -8,9 +8,11 @@
 import React, { useState, useReducer } from "react";
 import { useDispatch } from "react-redux";
 import {v4 as uuid_v4} from 'uuid';
+import Calendar from "react-calendar";
 
 // Styles
 import "./styles/CreatePostModal.css";
+import 'react-calendar/dist/Calendar.css';
 
 // Actions
 import * as _activityActions from '../../../store/actions/posting-project-user/activity/activity';
@@ -25,7 +27,10 @@ const AddActivityModal = (props) => {
   const [activityName, setActivityName] = useState("");
   const [activityDescription, setActivityDescription] = useState("");
   const [activityLocation, setActivityLocation] = useState("");
-  const [activityDate, setActivityDate] = useState(null);
+  const [activityDate, setActivityDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [activityImage, setActivityImage] = useState(null)
+  const [imageLoading, setImageLoading] = useState(false);
 
   // Dispatch 
   const dispatch = useDispatch();
@@ -77,7 +82,27 @@ const AddActivityModal = (props) => {
    * @returns {void}
    */
   const _onAddActivity = () => {
-    dispatch(_activityActions._addActivity_ppu(uuid_v4(), activityName, activityDescription,activityLocation, activityDate))
+    console.log('activityDate', activityDate)
+    dispatch(_activityActions._addActivity_ppu(uuid_v4(), activityName, activityDescription,activityLocation, activityDate.toDateString(), activityImage))
+    props.history.push('/projectInfo')
+  }
+
+  const _showCalendar = () => {
+    setShowCalendar(prevState => !prevState)
+  }
+
+  const _chooseImageHandle = (image) => {
+    const reader = new FileReader();
+    reader.onloadstart = () => setImageLoading(true)
+
+    reader.onload = event => {
+      console.log('event onload', event)
+      setActivityImage(event.target.result);
+      setImageLoading(false)
+    };
+  
+    reader.readAsDataURL(image[0]);
+    console.log('data', activityImage)
   }
 
   return (
@@ -109,15 +134,27 @@ const AddActivityModal = (props) => {
             onChange={(location) => _onChangeActivityLocation(location.target.value)}
           />
         </div>
-        <div className="boxModal">
-          <input 
-            type="text" 
-            placeholder="Date *"
+        <button className="boxModal-date" onClick={_showCalendar} >
+          Choose Date
+        </button>
+        {showCalendar && 
+          <Calendar
+            view='month'
             value={activityDate}
-            onChange={(date) => _onChangeActivityDate(date.target.value)}
+            onClickDay={_onChangeActivityDate}
+            activeStartDate={activityDate}
+            onDrillUp={() => console.log('drilled')}
           />
-        </div>
+        }
       </div>
+      <input
+          id="car"
+          type="file"
+          accept="image/*"
+          capture="camera"
+          onChange={img => _chooseImageHandle(img.target.files)}
+        />
+        <img src={activityImage} />
       <button 
         onClick={_onAddActivity}
       >
