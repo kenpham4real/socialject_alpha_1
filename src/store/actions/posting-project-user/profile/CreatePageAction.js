c2fe9e4ce4a2b053998b0855ac6fbd076ed938b4
Tran Ngoc Tien <ngoctien27122001@gmail.com>
2020-09-17 23:52:18 +0700

    edit code indent, delete some spaces, beautify the code

--- src/store/actions/posting-project-user/profile/CreatePageAction.js
+++ src/store/actions/posting-project-user/profile/CreatePageAction.js
@@ -13,91 +13,62 @@ export const CREATE_PROFILE = "CREATE_PROFILE";
 /******************************** ACTIONS ********************************/
 
   
-      /**
-   * @summary Handle upload the image to storage of Firebase
-   * @param {Object} imageFile An object containning such as {name:...,type:img/png,...vv}
-   * @return {void}
-   * @author TrNgTien
-   */
-const handleUpLoad = async (imageFile)=>{
-
-    // let imageUrl;
-   
-    const uploadTask = firebase_storage.ref(`images/${imageFile.name}`)
-
-    return new Promise(async (res, rej) => {
-        uploadTask.put(imageFile).on(
-            "state_changed",
-            snapshot =>{},
-            error =>{
-                console.log("big erroor",error);
-                rej(error)
-            },
-            ()=>{ 
-                firebase_storage
-                .ref("images")
-                .child(imageFile.name)
-                .getDownloadURL()
-                .then(url=>{
-                    // setUrl(url);
-                    console.log(url)
-                    res(url)
-                    });
-            }
-        );
-    })
-    
-        // return imageUrl ? imageUrl : "";
-        // ternary operator
-
-}
- 
-
-
- 
-
-
-
-export const _createProfile_ppu = (orgId, orgName, description, location, University,email,phoneNumber,facebook,imageFile) => {
-
-
-    
-
     /**
-     * @summary Asynchronous function calling the database to push the data
-     * @param {function} dispatch => Function used to send the action type and data to the Redux reducer
-     * @param {function} getState => Function used to access the current state of the application
-     * @returns {void}
+     * @summary Handle upload the image to storage of Firebase
+     * @param {Object} imageFile An object containning such as {name:...,type:img/png,...vv}
+     * @return {void}
      * @author TrNgTien
      */
-    return async (dispatch, getState) => {
-     
-        const imageUrl = await handleUpLoad(imageFile);
+    const handleUpLoad = async (imageFile)=>{
+    
+        const uploadTask = firebase_storage.ref(`images/${imageFile.name}`)
 
-        console.log('url in createProfile', imageUrl)
+        return new Promise(async (res, rej) => {
+            uploadTask.put(imageFile).on(
+                "state_changed",
+                snapshot =>{},
+                error =>{
+                    console.log("big erroor",error);
+                    rej(error)
+                },
+                ()=>{ 
+                    firebase_storage
+                    .ref("images")
+                    .child(imageFile.name)
+                    .getDownloadURL()
+                    .then(url=>{
+                        console.log(url)
+                        res(url)
+                        });
+                }
+            );
+        })
+        
+            
+    }
+ 
 
-        try {
-            await
-            firebase_db
-            .collection('organization')
-            .doc(`${testing_organization_id}`)
-            .set({
-                orgId:orgId,
-                orgName:orgName,
-                description:description,
-                location:location,
-                university:University,
-                email:email,
-                phoneNumber:phoneNumber,
-                facebook:facebook,
-                imageFile:imageUrl,
-            }, {merge: true})
 
-            console.log('Create profile successfully');
+ 
 
-            dispatch({
-                type: CREATE_PROFILE,
-                finishCreate: {
+
+
+    export const _createProfile_ppu = (orgId, orgName, description, location, University,email,phoneNumber,facebook,imageFile) => {
+        /**
+         * @summary Asynchronous function calling the database to push the data
+         * @param {function} dispatch => Function used to send the action type and data to the Redux reducer
+         * @param {function} getState => Function used to access the current state of the application
+         * @returns {void}
+         * @author TrNgTien
+         */
+        return async (dispatch, getState) => {
+            const imageUrl = await handleUpLoad(imageFile);
+            try {
+                await
+                firebase_db
+                .collection('organization')
+                .doc(`${testing_organization_id}`)
+                .set({
                     orgId:orgId,
                     orgName:orgName,
                     description:description,
@@ -106,12 +77,28 @@ export const _createProfile_ppu = (orgId, orgName, description, location, Univer
                     email:email,
                     phoneNumber:phoneNumber,
                     facebook:facebook,
-                    imageFile:imageFile,
-                }
-            })
-            
-        } catch (error) {
-            console.log('Error::', error);
+                    imageFile:imageUrl,
+                }, {merge: true})
+                console.log('Create profile successfully');
+
+                
+                dispatch({
+                    type: CREATE_PROFILE,
+                    finishCreate: {
+                        orgId:orgId,
+                        orgName:orgName,
+                        description:description,
+                        location:location,
+                        university:University,
+                        email:email,
+                        phoneNumber:phoneNumber,
+                        facebook:facebook,
+                        imageFile:imageFile,
+                    }
+                })
+                
+            } catch (error) {
+                console.log('Error::', error);
+            }
         }
-    }
 }
