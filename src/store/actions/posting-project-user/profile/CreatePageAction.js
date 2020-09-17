7021093595e3cb9dd41c7cc77dc8bc6eabbe34db
Tran Ngoc Tien <ngoctien27122001@gmail.com>
2020-09-17 23:49:44 +0700

    Create actions and connect action to reducer. Firstly,I add action push data text of PPU and save that to global state. Secondly, Ken and add action push image to storage of firebase and import url of the image. Finally, Ken and I add the url of the image into cloud firestore to fetch for next screen - Long's screen.

---
+++ src/store/actions/posting-project-user/profile/CreatePageAction.js
@@ -0,0 +1,117 @@
+// Firebase database
+import {firebase_db} from '../../../../firebase-config';
+import { firebase_storage } from "../../../../firebase-config";
+import { storage } from "firebase";
+
+// Constants
+import { testing_organization_id} from '../../../../constants/testing-keys';
+
+
+// Export all the action types
+export const CREATE_PROFILE = "CREATE_PROFILE";
+
+/******************************** ACTIONS ********************************/
+
+  
+      /**
+   * @summary Handle upload the image to storage of Firebase
+   * @param {Object} imageFile An object containning such as {name:...,type:img/png,...vv}
+   * @return {void}
+   * @author TrNgTien
+   */
+const handleUpLoad = async (imageFile)=>{
+
+    // let imageUrl;
+   
+    const uploadTask = firebase_storage.ref(`images/${imageFile.name}`)
+
+    return new Promise(async (res, rej) => {
+        uploadTask.put(imageFile).on(
+            "state_changed",
+            snapshot =>{},
+            error =>{
+                console.log("big erroor",error);
+                rej(error)
+            },
+            ()=>{ 
+                firebase_storage
+                .ref("images")
+                .child(imageFile.name)
+                .getDownloadURL()
+                .then(url=>{
+                    // setUrl(url);
+                    console.log(url)
+                    res(url)
+                    });
+            }
+        );
+    })
+    
+        // return imageUrl ? imageUrl : "";
+        // ternary operator
+
+}
+ 
+
+
+ 
+
+
+
+export const _createProfile_ppu = (orgId, orgName, description, location, University,email,phoneNumber,facebook,imageFile) => {
+
+
+    
+
+    /**
+     * @summary Asynchronous function calling the database to push the data
+     * @param {function} dispatch => Function used to send the action type and data to the Redux reducer
+     * @param {function} getState => Function used to access the current state of the application
+     * @returns {void}
+     * @author TrNgTien
+     */
+    return async (dispatch, getState) => {
+     
+        const imageUrl = await handleUpLoad(imageFile);
+
+        console.log('url in createProfile', imageUrl)
+
+        try {
+            await
+            firebase_db
+            .collection('organization')
+            .doc(`${testing_organization_id}`)
+            .set({
+                orgId:orgId,
+                orgName:orgName,
+                description:description,
+                location:location,
+                university:University,
+                email:email,
+                phoneNumber:phoneNumber,
+                facebook:facebook,
+                imageFile:imageUrl,
+            }, {merge: true})
+
+            console.log('Create profile successfully');
+
+            dispatch({
+                type: CREATE_PROFILE,
+                finishCreate: {
+                    orgId:orgId,
+                    orgName:orgName,
+                    description:description,
+                    location:location,
+                    university:University,
+                    email:email,
+                    phoneNumber:phoneNumber,
+                    facebook:facebook,
+                    imageFile:imageFile,
+                }
+            })
+            
+        } catch (error) {
+            console.log('Error::', error);
+        }
+    }
+}
