f6835cc7663a739b90bbf86690817855483d3369
unknown <marwinnortington@gmail.com>
2020-09-20 10:40:19 +0700

    Handle the UI and dispatch function in CreatePostModal_3

--- src/pages/app/posting_project_user/CreatePostModal_3.js
+++ src/pages/app/posting_project_user/CreatePostModal_3.js
@@ -10,38 +10,12 @@ import { useDispatch } from "react-redux";
 
 import "./styles/CreatePostModal_3.css";
 
-//Firebase stuff. Include this allow the PushData() function to run properly
-import { firebase_db } from "../../../firebase-config";
-import { _createProject_ppu } from "../../../store/actions/posting-project-user/project/project";
+// Actions
+import * as projectActions from "../../../store/actions/posting-project-user/project/project";
 
-//Function to pushdata on Firebase. The id of the object will be generated randomly
-function PushData(data, profileId) {
-  //To Organization
-  firebase_db
-    .collection("organization") //Go to the collection: organization
+// Components
+import {ImagePreview} from '../../../components/app/ImagePreview'
 
-    .add(data) //add a new object in that collection, with the data taken from the parameter.
-    //Those two next function are to make the debug/testing process easier.
-    .then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function (error) {
-      console.error("Error adding document: ", error);
-    });
-  //To public project
-  firebase_db
-    .collection("public-projects") //Go to the collection: public-project
-    .add(data) //add a new object in that collection, with the data taken from the parameter.
-    //Those two next function are to make the debug/testing process easier.
-    .then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function (error) {
-      console.error("Error adding document: ", error);
-    });
-}
-
-//Main function.
 const CreatePostModal_3 = (props) => {
   const dispatch = useDispatch();
 
@@ -51,6 +25,8 @@ const CreatePostModal_3 = (props) => {
 
   const [projectRequirementArray, setProjectRequirementArray] = useState([]);
   const [projectRequirement, setProjectRequirement] = useState("");
+  const [projectImage, setProjectImage] = useState("");
+  const [projectImageFile, setProjectImageFile] = useState(null);
 
   //Handling Add benefits and requirements event
 
@@ -67,22 +43,14 @@ const CreatePostModal_3 = (props) => {
     console.log("projectRequirementArray", projectRequirementArray);
   };
 
-  //The data to push to Fire Base. This is selected from the global store (Redux)
-  const postData = {
-    projectName: props.location.projectName,
-    projectDescription: props.location.projectDescription,
-    projectLocation: props.location.projectLocation,
-    projectDeadline: props.location.projectDeadline,
-    question: props.location.projectQuestionArray,
-    // benefit: projectBenefitArray,
-    // requirement: projectRequirementArray,
-    avatar: null,
-  };
-  console.log("Post Data: ", postData);
+  const {
+    projectName,
+    projectDescription,
+    projectLocation,
+    projectDeadline,
+    question
+  } = props.location;
 
-  //Make this to allow the the user to input the data.
-  const [temporary, setTemporary] = useState(null);
-  console.log("Rendering repeated"); //For testing.
   return (
     <div id="createPostModal_3">
       <h1>Bring it to everyone</h1>
@@ -146,29 +114,34 @@ const CreatePostModal_3 = (props) => {
           </li> */}
         </ul>
       </div>
-      <div className="avatarBox"> Avatar</div>
-      {/*<Link> </Link> component is to navigate*/}
-      {/* <Link to="/projectInfo" className="profile-link"> */}
+
+      <ImagePreview
+        image={projectImage}
+        setImage={setProjectImage}
+        setImageFile={setProjectImageFile}
+      />
+
       <button
         className="profile-button"
         onClick={
           //Now push the data onto Firebase.
           () =>
             dispatch(
-              _createProject_ppu(
-                postData,
+              projectActions._createProject_ppu(
+                projectName,
+                projectDescription,
+                projectLocation,
+                projectDeadline,
                 projectBenefitArray[projectBenefitArray.length - 1],
-                projectRequirementArray[projectRequirementArray.length - 1]
+                projectRequirementArray[projectRequirementArray.length - 1],
+                projectImageFile,
               )
             )
         }
       >
         Continue
       </button>
-      {/* </Link> */}
-      {/* <Link to="/CreatePostModal_2" className="profile-link"> */}
       <button className="profile-button">Go Back</button>
-      {/* </Link> */}
     </div>
   );
 };
