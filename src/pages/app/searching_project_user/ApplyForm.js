808df55f938f1e31ecdfe7c093bdae8acb562632
Tran Ngoc Tien <ngoctien27122001@gmail.com>
2020-09-21 00:12:48 +0700

    Created file to handle state and push the data of the form

--- src/pages/app/searching_project_user/ApplyForm.js
+++ src/pages/app/searching_project_user/ApplyForm.js
@@ -5,12 +5,62 @@
  */
 
  //Packages
- import React from "react";
+ import React, {useState} from "react";
+ import { useDispatch } from "react-redux";
+ import { upDataForm } from "../../../store/actions/searching-project-user/project/project";
 
  //Styles
  import "./styles/ApplyFormStyles.css";
 
+
  const ApplyForm = (props) => {
+    //Initialize the states
+    const [Name, setName] = useState("");
+    const [Email, setEmail] = useState("");
+    const [Message, setMessage] = useState("");
+
+    //Dispatch
+    const dispatch = useDispatch();
+
+    const FormState ={
+        Name:Name,
+        Email:Email,
+        Message:Message,
+    }
+   
+    /**
+     * @summary Handle State of Name
+     * @param {string} Name
+     * @return {void}
+     */
+    const onChangeName=(Name) =>{
+        setName(Name);
+    }
+
+    
+    /**
+     * @summary Handle State of Email
+     * @param {string} Email
+     * @return {void}
+     */
+    const onChangeEmail=(Email) =>{
+        setEmail(Email);
+    }
+
+
+    /**
+     * @summary Handle State of Message
+     * @param {string} Message
+     * @return {void}
+     */
+    const onChangeMessage=(Message) =>{
+        setMessage(Message);
+    }
+
+    const submitForm =() =>{
+        console.log("Sumbit form succesful!");
+        dispatch(upDataForm(FormState.Name,FormState.Email,FormState.Message));
+    }
     return (
         <div className="page" >
             <h1 className="h1">Project's Name</h1>
@@ -28,6 +78,8 @@
                             id="name" 
                             type="text" 
                             className="contact-form-input" 
+                            value={Name}
+                            onChange={(Name)=> onChangeName(Name.target.value)}
                         />
                     </div>
                     <div className="contact-form-group">
@@ -40,15 +92,38 @@
                         <input 
                             id="email" 
                             type="email" 
-                            className="contact-form-input" />
+                            className="contact-form-input"
+                            value={Email}
+                            onChange={(Email)=>onChangeEmail(Email.target.value)} 
+                        />
                     </div>
                         <div className="contact-form-group">
-                        <label for="message" className="contact-form-label">Your Message</label>
-                        <textarea name="message" id="message" className="contact-form-area" placeholder="Type something if you want"></textarea>
+                        <label 
+                            for="message" 
+                            className="contact-form-label"
+                        >
+                            Your Message
+                        </label>
+                        <textarea 
+                            name="message" 
+                            id="message" 
+                            className="contact-form-area" 
+                            placeholder="Type something if you want"
+                            value={Message}
+                            onChange={(Message)=>onChangeMessage(Message.target.value)}
+                        >
+
+                        </textarea>
                     </div>
                     <button 
                         type="submit" 
                         className="contact-form-submit"
+                        onClick={() => {
+                            submitForm();
+                            props.history.push({
+                              pathname: "/projectInfo",
+                            });
+                          }}
                     >
                         Submit
                     </button>
