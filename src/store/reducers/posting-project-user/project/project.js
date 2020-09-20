39ff4ce6d185d33e661c7d21bb0bdc0f74d8e272
Tran Ngoc Tien <ngoctien27122001@gmail.com>
2020-09-21 00:21:20 +0700

    Pulled this file from Ken

--- src/store/reducers/posting-project-user/project/project.js
+++ src/store/reducers/posting-project-user/project/project.js
@@ -1,23 +1,16 @@
 // Action types
 import {
-  SET_PROJECT,
   SET_PROJECT_RECRUIT_INFO,
-  SET_PROJECT_BASIC_INFO,
+  SET_PROJECT_BASIC_INFO
 } from "../../../actions/posting-project-user/project/project";
 
 // Initialize the state
 const initialState = {
-  projects: [],
-  postData: {
-    name: null,
-    description: null,
-    location: null,
-    deadline: null,
-    question: null,
-    benefit: [],
-    requirement: [],
-    avatar: null,
-  },
+  projects_basic_info: [],
+  projects_recruit_info: {
+    benefits: [],
+    requirements: []
+  }
 };
 /**
  * @summary Handle the states of the projects (initialState above)
@@ -28,65 +21,20 @@ const initialState = {
  */
 export const projectReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_PROJECT:
-      return {
+    case SET_PROJECT_BASIC_INFO:
+      return{
         ...state,
-        projects: action.projectData,
-      };
-    case "ADD_NAME":
-      console.log("Name dispatched.");
-      return {
+        projects_basic_info: state.projects_basic_info.concat(action.projectData)
+      }
+    case SET_PROJECT_RECRUIT_INFO:
+      console.log('action.benefits', action.benefits)
+      return{
         ...state,
-        postData: {
-          name: action.payload,
-        },
-      };
-    case "ADD_DESCRIPTION":
-      console.log("Description dispatched.");
-      return {
-        ...state,
-        postData: {
-          description: action.payload,
-        },
-      };
-    case "ADD_LOCATION":
-      console.log("Location dispatched.");
-      return {
-        ...state,
-        postData: {
-          location: action.payload,
-        },
-      };
-    case "ADD_DEADLINE":
-      console.log("Deadline dispatched.");
-      return {
-        ...state,
-        postData: {
-          deadline: action.payload,
-        },
-      };
-    /* For later use. Haven't finished this :))))
-    case ADD_QUESTION:
-      return {
-        ...state,
-        question: action.payload,
-      };
-    case ADD_BENEFIT:
-      return {
-        ...state,
-        benefit: action.payload,
-      };
-    case ADD_REQUIREMENT:
-      return {
-        ...state,
-        requirement: action.payload,
-      };
-    case ADD_AVATAR:
-      return {
-        ...state,
-        avatar: action.payload,
-      };
-      */
+        projects_recruit_info: {
+          benefits: action.benefits,
+          requirements: action.requirements
+        }
+      }
     default:
       return state;
   }
