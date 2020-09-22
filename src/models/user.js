83037827358e84947cd28325b2f838200757b858
unknown <marwinnortington@gmail.com>
2020-09-22 23:57:13 +0700

    Create the User class

---
+++ src/models/user.js
@@ -0,0 +1,10 @@
+export class User{
+    constructor(uid, token, userType, userEmail, userAvatar, userName){
+        this.uid = uid;
+        this.token = token;
+        this.userType = userType;
+        this.userEmail = userEmail;
+        this.userAvatar = userAvatar;
+        this.userName = userName
+    }
+}
