7ac0b3c1078668fbf515d290b8c7440da4f64eaa
Tran Ngoc Tien <ngoctien27122001@gmail.com>
2020-10-01 23:11:14 +0700

    add useSelector becuz when i pulled the code it take an error of missing useSelector

--- src/pages/app/posting_project_user/CreatePostModal_3.js
+++ src/pages/app/posting_project_user/CreatePostModal_3.js
@@ -6,7 +6,7 @@
 */
 
 import React, { Component, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 import {v4 as uuid_v4} from 'uuid'
 
 // Styles
