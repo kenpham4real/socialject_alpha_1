ddb35d481c16766a223d24a11924c8630b2949c7
unknown <marwinnortington@gmail.com>
2020-10-05 14:59:49 +0700

    Temporary form submission

--- src/components/app/ProjectInfoPage/FormSubmission.js
+++ src/components/app/ProjectInfoPage/FormSubmission.js
@@ -1,49 +1,43 @@
 import React from "react";
 
 import "../../styles/ProjectInfoPage/FormSubmission.css";
-import Modal from "react-modal";
 
 const SubmissionCard = (props) => {
-  const formSubmissionData = props.formData;
-  console.log("Form Submission Data", formSubmissionData);
+  const applicantInfo = props.applicantInfo;
+
+  console.log('applicantInfo', applicantInfo)
+
   return (
-    <div className="formsub-card">
-      <img
-        src={formSubmissionData.avatar}
-        alt="Loading..."
-        className="formsub-avatar"
-        onClick={() => props.onModalOpening()}
-      />
-      <div
-        className="formsub-textcontainer"
-        onClick={() => console.log("Modal's username clicked")}
-      >
-        <div className="formsub-text title">{formSubmissionData.name}</div>
-        <div className="formsub-text">{formSubmissionData.email}</div>
+    <div className="formsub-card" onClick={() => props._onViewStudentAnswer(props.applicantInfo)}>
+      <img src={applicantInfo.studentInfo.studentAvatar} alt="Loading..." className="formsub-avatar" />
+      <div className="formsub-textcontainer">
+        <div className="formsub-text title">{applicantInfo.studentInfo.studentName}</div>
+        <div className="formsub-text">{applicantInfo.studentInfo.studentEmail}</div>
       </div>
     </div>
   );
 };
 
 const FormSubmission = (props) => {
-  //These data are just example.
-  const imageURL =
-    "https://i.pinimg.com/originals/39/46/55/39465510117c36c2023b2d72cdcf05b3.jpg";
-  const formSubmissions = props.formData;
-  console.log("Form Submission", formSubmissions);
-  // if (props.userId == props.projectOwnerId)
-  return (
-    <div className="formsub-container">
-      <div className="formsub-text title big"> Form Submissions</div>
-      {formSubmissions.map((formSubmissionData) => (
-        <SubmissionCard
-          formData={formSubmissionData}
-          onModalOpening={() => props.onModalOpening(formSubmissionData)}
-        />
-      ))}
-    </div>
-  );
-  // return <div style={{ display: "none" }}></div>;
+  console.log('props in form submission', props)
+  console.log('props.userId: ', props.userId, "vs props.projectOwnerId", props.projectOwnerId)
+  if (props.userId == props.projectOwnerId){
+
+    return (
+      <div className="formsub-container">
+        <div className="formsub-text title big"> Form Submissions</div>
+        {props.isFetchedRecruitInfo && props.formSubmissions.map((formSubmission) => (
+          <SubmissionCard
+            applicantInfo={formSubmission}
+            onModalOpening={() => props.onModalOpening(formSubmissionData)}
+          />
+        ))}
+      </div>
+    );
+  }
+  return <div style={{ display: "none" }}></div>;
 };
 
 export default FormSubmission;
+
+
