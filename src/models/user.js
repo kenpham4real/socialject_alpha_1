ba455a21b701700b9af3097739e27c3948221fda
unknown <marwinnortington@gmail.com>
2020-10-05 11:08:51 +0700

    Fix authentication and auto authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,22 @@
+import { ADMIN } from "../constants/user-types";
+
 export class User{
     constructor(uid, token, userType, userEmail, userAvatar, userName, authenticatedMethod, isAuth=false){
         this.uid = uid;
         this.token = token;
         this.userType = userType;
-        this.userEmail = userEmail;
-        this.userAvatar = userAvatar;
-        this.userName = userName;
         this.authenticatedMethod = authenticatedMethod;
         this.isAuth = isAuth;
+
+        if(userType === ADMIN){
+            this.email = userEmail;
+            this.orgAvatar = userAvatar;
+            this.orgName = userName;
+        }else{
+            this.userEmail = userEmail;
+            this.userAvatar = userAvatar;
+            this.userName = userName;
+        }
+
     }
 }
