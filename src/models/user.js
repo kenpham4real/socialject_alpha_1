34b55f5336ebedbc428d3b55c427fd5f08aea491
unknown <marwinnortington@gmail.com>
2020-10-05 23:41:17 +0700

    Fix User model

--- src/models/user.js
+++ src/models/user.js
@@ -2,17 +2,18 @@ import { ADMIN } from "../constants/user-types";
 
 export class User{
     constructor(uid, token, userType, userEmail, userAvatar, userName, authenticatedMethod, isAuth=false){
-        this.uid = uid;
         this.token = token;
         this.userType = userType;
         this.authenticatedMethod = authenticatedMethod;
         this.isAuth = isAuth;
 
         if(userType === ADMIN){
+            this.orgId = uid;
             this.email = userEmail;
             this.orgAvatar = userAvatar;
             this.orgName = userName;
         }else{
+            this.userId = uid
             this.userEmail = userEmail;
             this.userAvatar = userAvatar;
             this.userName = userName;
