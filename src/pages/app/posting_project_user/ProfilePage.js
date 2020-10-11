efa2d3ca88985e58d0086d27592796909c467e81
66952546 <luuhienlong201@gmail.com>
2020-10-11 18:16:42 +0700

    Fixed the profile page. Now it can always fetch data (base on the Id from local storage).

--- src/pages/app/posting_project_user/ProfilePage.js
+++ src/pages/app/posting_project_user/ProfilePage.js
@@ -53,10 +53,7 @@ function AddProjectButton(props) {
 
   if (userId === props.projectOwnerId)
     return (
-      <Link
-        to={`/createPostModal`}
-        className="profile-button"
-      >
+      <Link to={`/createPostModal`} className="profile-button">
         Add a project
       </Link>
     );
@@ -182,16 +179,23 @@ const ProfilePage = (props) => {
    * @param {void}
    * @returns {profileData and projectData} (The data is selected from global store)
    */
-  //Get the Id from history.location
-  const profileId = props.history.location.profileId;
-  console.log("props", props);
+  //Get the Id from history.location (old code)
+  // const profileId = props.history.location.profileId;
+  // console.log("props", props);
+
+  //Get id from user data (local storage)
+  let user = JSON.parse(localStorage.getItem("userData"));
+  let profileId;
+  if (user) profileId = user.userId;
   console.log("profile Id received is: ", profileId);
+
   //This will not fetch if there is no if passed into it.
   //Declare hooks as variables to be more flexible.
   const dispatch = useDispatch();
   //Select data from the global state.
   const projectData = useSelector((state) => state.profileReducer.projectArray);
   const profileData = useSelector((state) => state.profileReducer.profileData);
+
   //Use 2 hooks useEffect and useCallback to prevent re-render, but it still re-render anyway.
   /*const fetchCallback = profileAction.FetchCalling(
     profileAction.FetchProject,
@@ -200,6 +204,7 @@ const ProfilePage = (props) => {
     callback,
     profileId
   );*/
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const fetchCallback = useCallback(() =>
     profileAction.FetchProject(dispatch, profileId)
