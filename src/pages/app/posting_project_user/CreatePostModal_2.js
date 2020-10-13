048cbd1ce73fe006d578d04ab38df240008a28c0
Tran Ngoc Tien <ngoctien27122001@gmail.com>
2020-10-14 00:14:10 +0700

    get the state to push states of 2 pages to push the last page

--- src/pages/app/posting_project_user/CreatePostModal_2.js
+++ src/pages/app/posting_project_user/CreatePostModal_2.js
@@ -4,13 +4,18 @@
 
 
 */
-
+//Packages
 import React, { useState } from "react";
-import { FormInput } from "../../../components/app/Form/FormInput";
+// import { FormInput } from "../../../components/app/Form/FormInput";
+import { IoIosAdd } from "react-icons/io";
+
+//Styles
 import "./styles/CreatePostModal_2.css";
+
 
 const CreatePostModal_2 = (props) => {
 
+  console.log("props",props)
   const [questionCount, setQuestionCount] = useState([0]);
   const [questions, setQuestions] = useState([""]);
 
@@ -34,49 +39,66 @@ const CreatePostModal_2 = (props) => {
     setQuestions(allQuestions);
   }
 
+  const createPostModal={
+    projectName:props.location.projectName,
+    projectDescription:props.location.projectDescription,
+    projectLocation:props.location.projectLocation,
+    projectCategory:props.location.projectCategory,
+    projectDate:props.location.projectDate,
+    showCalendar:props.location.showCalendar,
+    projectImageFile:props.location.projectImageFile
+  }
 
-  const _onChangeHandleContinue = () => {
-    props.history.push({
-      pathname: "/createPostModal_3",
-      projectName: props.location.state.projectName,
-      projectDescription: props.location.state.projectDescription,
-      projectLocation: props.location.state.projectLocation,
-      projectDeadline: props.location.state.projectDeadline,
-      projectQuestions: questions
-    });
-  };
 
   return (
     <div id="createPostModal_2">
-      <h1>Recruiting your Members</h1>
-      <p>Create a form with simple questions for your applicants</p>
+      <h1 className="h1-post-modal-2">Recruiting your Members</h1>
+      <p className="p-post-modal-2">Create a form with simple questions for your applicants</p>
       <div class="addQuestionBox">
         <div>
-          Question
-          <div class="addQuestionButton">
-
-            <button onClick={_onAddQuestionInput} >Add your question</button>
+          <p className="question">Question</p>
+          
+          <div >
+            <i>
+              <IoIosAdd
+                className="add-question"
+                onClick={_onAddQuestionInput} 
+              />
+            </i>
             <ul>
               <li>
+                
                 {questionCount.map((index) => (
-                  <input
-                    placeholder="Your question"
-                    value={questions[index]}
-                    onChange={(text) => _onChangeQuestion(text.target.value, index)}
-                  />
+                    <li>
+                      <input
+                      className="form-input"
+                      placeholder="Your question"
+                      value={questions[index]}
+                      onChange={(text) => _onChangeQuestion(text.target.value, index)}
+                      />
+                    </li>
                 ))}
               </li>
             </ul>
            </div>
         </div>
       </div>
-      <button
+      <div
+      className="continue-post-modal"
         onClick={() => {
-          _onChangeHandleContinue();
+          createPostModal();
+          props.history.push({
+            pathname: "/createPostModal_3",
+            projectName: props.location.state.projectName,
+            projectDescription: props.location.state.projectDescription,
+            projectLocation: props.location.state.projectLocation,
+            projectDeadline: props.location.state.projectDeadline,
+            projectQuestions: questions
+          });
         }}
       >
         Continue
-      </button>
+      </div>
     </div>
   );
 };
