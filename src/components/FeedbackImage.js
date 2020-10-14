e7a0669cb18ee8c9478db92230fd9c6e1bee6241
unknown <marwinnortington@gmail.com>
2020-10-14 22:55:44 +0700

    FeedbackImage component

--- src/components/FeedbackImage.js
+++ src/components/FeedbackImage.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
+// Constants
+import {FEEDBACKS} from '../constants/feedback'
+
 // Styles
 import './styles/FeedbackImage.css'
 
 const FeedbackImage = props => {
+
+    const feedbackInfo = FEEDBACKS[Math.floor(Math.random()*FEEDBACKS.length)]
+
     return(
         <div className="illustration-wrapper">
-            <img alt="" className="illustration__image--person" src={require('../assets/images/ken3.png')} />
+            <img alt="" className="illustration__image--person" src={feedbackInfo["avatar"]} />
             <div className="illustration__feedback-wrapper">
                 <img alt="" className="illustration__image--quote" src={require('../assets/images/left-quote-white.png')} />
-                <p className="illustration__feedback" >SocialJect has given me the opportunity to grow by helping me find great clubs and organization</p>
-                <p className="illustration__feedback--name" >-Ken Pham-</p>
+                <p className="illustration__feedback" >{feedbackInfo["feedback"]}</p>
+                <p className="illustration__feedback--name" >-{feedbackInfo["name"]}-</p>
             </div>
         </div>
     )
